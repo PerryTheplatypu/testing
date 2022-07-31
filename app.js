62d50745b2f455f0747e2ee5b10caad481add747
PerryTheplatypu <110355249+PerryTheplatypu@users.noreply.github.com>
2022-07-31 16:09:18 -0500

    implementing the first function

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const colors = ['red','yellow','blue','green','brown'];
+
+const show_colors = (colors_array)=>{
+    for (const color of colors_array) {
+        console.log(color);
+    }
+};
+
+show_colors(colors);
